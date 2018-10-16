e9c383dff232d18f32047b38cca7b78cebd04a4e
flymanflyman <406544970@qq.com>
2018-10-16 23:57:13 +0800

    20180927 from home.

--- src/main/java/com/lh/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/lh/eurekaserver/EurekaServerApplication.java
@@ -10,5 +10,6 @@ public class EurekaServerApplication {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(EurekaServerApplication.class).web(true).run(args);
+
     }
 }
